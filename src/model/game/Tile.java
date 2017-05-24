22d0911e535f1fe45c0a8687b1acbbab082307b3
FederAndInk <federandink@gmail.com>
2017-05-24 10:20:30 +0200

    correct indent to tile

--- src/model/game/Tile.java
+++ src/model/game/Tile.java
@@ -1,19 +1,21 @@
 package model.game;
 
 public class Tile {
-
-	private Coords coords;
-	private Site site;
-	private TileState state;
-
-	public void shoreUp() {
-		// TODO - implement Tile.shoreUp
-		throw new UnsupportedOperationException();
-	}
-
-	public boolean canBeShoredUp() {
-		// TODO - implement Tile.canBeShoredUp
-		throw new UnsupportedOperationException();
-	}
-
+    
+    private Coords    coords;
+    private Site      site;
+    private TileState state;
+    
+    
+    public void shoreUp() {
+        // TODO - implement Tile.shoreUp
+        throw new UnsupportedOperationException();
+    }
+    
+    
+    public boolean canBeShoredUp() {
+        // TODO - implement Tile.canBeShoredUp
+        throw new UnsupportedOperationException();
+    }
+    
 }

2518ca5444e920f6a71f1420a4b1f530ccfc3aec
FederAndInk <federandink@gmail.com>
2017-05-29 11:27:52 +0200

    add some utils functions to game

--- src/model/game/Game.java
+++ src/model/game/Game.java
@@ -1,5 +1,6 @@
 package model.game;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 import model.player.Player;
@@ -7,14 +8,14 @@ import model.player.Player;
 
 
 public class Game {
-    
-    private Collection<Treasure> treasures;
+    private final static Integer MAX_PLAYER = 4;
+    private ArrayList<Treasure>  treasures;
     private Island               island;
     private Collection<Player>   players;
     private Deck                 treasureDeck;
     private Deck                 floodDeck;
     private Player               currentPlayer;
-    private Integer              gameID;
+    private boolean              started;
     
     
     public Game getcurrentGame() {
@@ -23,8 +24,42 @@ public class Game {
     }
     
     
+    /**
+     * @author nihil
+     * add a player to the game (4 max)
+     * @param p
+     * the player to add to the party
+     * @return the number of players after adding they
+     */
+    public Integer addPlayer(Player p) {
+        if (players.size() < MAX_PLAYER) {
+            players.add(p);
+        } // end if
+        return players.size();
+    }// end addPlayer
+    
+    
     public Player getCurrentPlayer() {
         return this.currentPlayer;
+    }
+    
+    
+    public boolean isStarted() {
+        return started;
+    }
+    
+    
+    /**
+     * <p>
+     * then, there is no more treasures in game
+     * </p>
+     * 
+     * @author nihil
+     *
+     * @return true if the players get all the treasures
+     */
+    private boolean isTreasureAllInveked() {
+        return treasures.isEmpty();
     }
     
 }

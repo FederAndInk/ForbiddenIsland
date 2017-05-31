bd8bca99b32b0407d126889c5b5fa930babe9976
ChatonRêveur <vincent.marechal.pro@gmail.com>
2017-05-31 09:46:24 +0000

    [not Functional] special move Diver

--- src/model/adventurers/Diver.java
+++ src/model/adventurers/Diver.java
@@ -2,15 +2,57 @@ package model.adventurers;
 
 import java.util.ArrayList;
 import model.game.Tile;
+import model.game.TileState;
 import model.player.Player;
 
 
 
 public class Diver extends Adventurer {
     
+    private ArrayList<Tile> getReachableTiles(Tile tile, ArrayList<Tile> tilesAlreadyRead) {
+        ArrayList<Tile> tilesAlreadyDone = new ArrayList<>();
+        
+        Tile[][] grid = getPlayer().getCurrentGame().getIsland().getGrid();
+        
+        for (int i = -1; i <= 1; i += 2) {
+            Tile tileTmp = (grid[tile.getCoords().getX()][tile.getCoords().getY() + i]);
+            if ((tileTmp != null) && (tileTmp.getState() != TileState.SINKED)) {
+                tilesAlreadyRead.add(tileTmp);
+            } else if ((tileTmp != null) && (tileTmp.getState() == TileState.SINKED)) {
+                if (!tilesAlreadyRead.contains(tileTmp)) {
+                    tilesAlreadyRead.add(tileTmp);
+                    tilesAlreadyDone.addAll(getReachableTiles(tileTmp, tilesAlreadyRead));
+                }
+            }
+            tileTmp = (grid[tile.getCoords().getX() + i][tile.getCoords().getY()]);
+            if ((tileTmp != null) && (tileTmp.getState() != TileState.SINKED)) {
+                tilesAlreadyRead.add(tileTmp);
+            } else if ((tileTmp != null) && (tileTmp.getState() == TileState.SINKED)) {
+                if (!tilesAlreadyRead.contains(tileTmp)) {
+                    tilesAlreadyRead.add(tileTmp);
+                    tilesAlreadyDone.addAll(getReachableTiles(tileTmp, tilesAlreadyRead));
+                }
+            }
+        }
+        return tilesAlreadyDone;
+    }
+    
+    
+    @Override
     public ArrayList<Tile> getReachableTiles() {
-        // TODO - implement Diver.getReachableTiles
-        throw new UnsupportedOperationException();
+        ArrayList<Tile> reachableTiles = new ArrayList<>();
+        ArrayList<Tile> tilesAlreadyDone = new ArrayList<>();
+        Tile current = getCurrentTile();
+        
+        tilesAlreadyDone = getReachableTiles(current, tilesAlreadyDone);
+        
+        for (Tile tile : tilesAlreadyDone) {
+            if (tile.getState() != TileState.SINKED) {
+                reachableTiles.add(tile);
+            }
+        }
+        
+        return reachableTiles;
     }
     
     

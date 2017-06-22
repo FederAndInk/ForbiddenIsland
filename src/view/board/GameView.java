315a8e09c0fc03cd2156618e21c38a90a0632a2a
Azeris38 <moreausimon38@gmail.com>
2017-06-22 13:16:01 +0000

    Button add provisory

--- src/view/board/GameView.java
+++ src/view/board/GameView.java
@@ -76,6 +76,10 @@ public class GameView extends JFrame {
     private JButton moveBtn;
     private JButton shoreUpBtn;
     private JButton useCapacityBtn;
+    private JButton discardCard;
+    private JButton invoque;
+    private JButton giveCard;
+    private JButton useCard;
     
     private ListenerAction listObs;
     
@@ -103,7 +107,7 @@ public class GameView extends JFrame {
         
         eastPane = new JPanel(new BorderLayout());
         westPane = new JPanel(new BorderLayout());
-        actionCommands = new JPanel(new GridLayout(4, 1));
+        actionCommands = new JPanel(new GridLayout(8, 1));
         
         bar = new JMenuBar();
         option = new JMenu("Options");
@@ -124,13 +128,17 @@ public class GameView extends JFrame {
         GridBagLayout layout = new GridBagLayout();
         paneDroit.setLayout(layout);
         layout.rowHeights = new int[2];
-        double[] weight = { 0.15, 0.85 };
+        double[] weight = { 0.1, 0.8 };
         layout.rowWeights = weight;
         
         endTurnBtn = new JButton("Fin de tour");
         moveBtn = new JButton("Se déplacer");
         shoreUpBtn = new JButton("Assécher un endroit");
         useCapacityBtn = new JButton("Utiliser sa capacité");
+        discardCard = new JButton("Défausser une carte");
+        invoque = new JButton("Invoquer un trésor");
+        giveCard = new JButton("Donner une carte");
+        useCard = new JButton("Utiliser une carte");
         
         floodCursor = new WaterRise(2);
         
@@ -194,7 +202,16 @@ public class GameView extends JFrame {
         actionCommands.add(moveBtn);
         actionCommands.add(shoreUpBtn);
         actionCommands.add(useCapacityBtn);
-        
+        actionCommands.add(discardCard);
+        actionCommands.add(giveCard);
+        actionCommands.add(useCard);
+        actionCommands.add(invoque);
+        for (Component c : actionCommands.getComponents()) {
+            if (c != null) {
+                c.setFont(new Font(c.getFont().getFontName(), c.getFont().getStyle(),
+                        (int) (c.getFont().getSize() * 0.7)));
+            }
+        }
     }
     
     

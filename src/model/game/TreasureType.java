6154efc568cd9c316eff141bf7fc7f389620d532
FederAndInk <federandink@gmail.com>
2017-06-22 09:11:31 +0200

    add treasureComponent

--- src/model/game/TreasureType.java
+++ src/model/game/TreasureType.java
@@ -3,19 +3,33 @@ package model.game;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import util.LogType;
+import util.Parameters;
+
 
 
 public enum TreasureType {
-    EARTH_STONE("The Earth Stone"),
-    STATUE_OF_THE_WIND("The Statue Of The Wind"),
-    CRYSTAL_OF_FIRE("The Crystal Of Fire"),
-    OCEANS_CHALICE("The Ocean's Chalice");
+    EARTH_STONE("Earth Stone"),
+    STATUE_OF_THE_WIND("Statue of the Wind"),
+    CRYSTAL_OF_FIRE("Crystal of Fire"),
+    OCEANS_CHALICE("Ocean's Chalice");
     
     private String label;
     
     
     private TreasureType(String label) {
         this.label = label;
+    }
+    
+    
+    /**
+     * @author nihil
+     *
+     */
+    public String getPath() {
+        Parameters.printLog("get " + Parameters.TREASURES + "Treasure_" + label.replace(" ", "_") + "@2x.png",
+                LogType.ACCESS);
+        return Parameters.TREASURES + "Treasure_" + label.replace(" ", "_") + "@2x.png";
     }
     
     
--- src/view/board/TreasureComponent.java
+++ src/view/board/TreasureComponent.java
@@ -3,6 +3,12 @@
  */
 package view.board;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import model.game.TreasureType;
@@ -14,5 +20,25 @@ import model.game.TreasureType;
  *
  */
 public class TreasureComponent extends JPanel {
-    TreasureType
+    TreasureType type;
+    
+    
+    public TreasureComponent(TreasureType type) {
+        this.type = type;
+    }
+    
+    
+    /**
+     * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+     */
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        try {
+            BufferedImage bi = ImageIO.read(new File(type.getPath()));
+            g.drawImage(bi, 0, 0, (int) getSize().getWidth(), (int) getSize().getHeight(), this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
